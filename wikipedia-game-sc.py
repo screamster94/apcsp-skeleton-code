16289c93d2c7af0a203f5e6428d91380f183ed11
Kareem Awad <screamster94@gmail.com>
2023-10-18 21:10:23 +0000

    Hola

--- wikipedia-game-sc.py
+++ wikipedia-game-sc.py
@@ -23,10 +23,36 @@ def wikipedia_game_solver(start_page, target_page):
     visited = []
     queue = Queue()
     path = []
+    parent = {}
 
     queue.put(start_page.title)
+    visited.append(start_page.title)
 
-    while not queue.empty()
+
+    while not queue.empty():
+        
+        current_title = queue.get()
+
+        if current_title == target_page.title:
+            break
+
+        #visit my current title
+        visited.append(current_title)
+        current_page = wiki_wiki.page(current_title)
+        next_level = fetch_links(current_page)
+
+        for node in next_level:
+            if node not in visited:
+                queue.put(node)
+                parent[node] = current_title
+        
+    child = target_page.title
+    while child != start_page.title:
+        path.append(child)
+        child = parent[child]
+    path.append(start_page.title)
+    path.reverse()
+        
         #get from queue (queue.get())
         #add to visited
         #fetch the links
